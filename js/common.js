1c0c3d98037d00ce201d4a60f453ac053da29a8e
Olesia <oles.ivanenko@gmail.com>
2016-11-25 05:54:28 +0200

    mogo
    second portfolio project

---
+++ js/common.js
@@ -0,0 +1,19 @@
+
+//slick
+
+$(document).ready(function(){
+	$('.single-item').slick({
+		infinite: true,
+		
+		
+		arrows: true,
+		// centerMode: true,
+		// variableWidth: true,
+		slidesToShow: 1
+
+
+	});
+
+});
+
+
